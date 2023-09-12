6f48823b08d19be19f0385ed0b560c8788b4fa71
tanut <tanut@2c2p.com>
2023-09-12 18:00:10 +0700

    Update

--- cypress/e2e/3-test/POST2.cy.js
+++ cypress/e2e/3-test/POST2.cy.js
@@ -5,14 +5,16 @@ describe('POST Request', () => {
     cy.fixture('body.json').then((body) => {
       const isoTimestamp = new Date().toISOString();
       const newGuid = uuidv4();
+      const neworderno = Math.floor(Math.random() * 100000) + 1;
       body.apiRequest.requestMessageID = newGuid;
       body.apiRequest.requestDateTime = isoTimestamp;
+      body.orderNo = neworderno;
       cy.request({
         method: 'POST',
         url: 'https://core.demo-paco.2c2p.com/api/1/Payment/prePaymentUi', // Replace with your API URL
         headers: {
           'Content-Type': 'application/json',
-          'apiKey':'bf474356f3094fb5b1830d4098352e9c'
+          'apiKey':'709d466ca455493197227f7051eab3c0'
           // Add any other headers you might need
         },
         body: body, // Use the loaded JSON from the file
@@ -25,6 +27,23 @@ describe('POST Request', () => {
         cy.visit(response.body.data.paymentPage.paymentPageURL);
         cy.wait(3000);
       });
+      // cy.get(':nth-child(1) > app-form-control > div > .global-form-control').type('4111111111111111')
+
+      // cy.get(':nth-child(2) > app-form-control > div > .global-form-control').type('1223')
+
+      // cy.get(':nth-child(3) > app-form-control > div > .global-form-control').type('123')
+
+      // cy.get('.cardholder-name > app-form-control > div > .global-form-control').type('Cypress')
+
+      // cy.get('.secondary-btn > .main-content-button').click()
+
+      // cy.get('.secondary-btn > .main-content-button').click()
+
+      // cy.get('.proceed').click()
+
+      // cy.get('.form-control').type('123456')
+
+      //cy.get('.acs-challenge-form-actions > .proceed').click()
     });
   }
   });
